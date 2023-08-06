a263959b55ab374b034a2cce8c246428a4cd48b3
Oussama WR <87127089+OussamaWR@users.noreply.github.com>
2023-08-06 18:25:08 +0100

    Update jwt.interceptor.ts
    delete comments

--- src/app/services/jwt.interceptor.ts
+++ src/app/services/jwt.interceptor.ts
@@ -14,13 +14,9 @@ export class JwtInterceptor implements HttpInterceptor {
   constructor(private tokenService : TokenService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    //mecanisme li kay dir override y9dar ybdal f header dyal n'import quel requete http kharja  mn angular
-    // bach  n9ad ndir les executer  les  api rest dyali khas  darori ykon 3ndi token fl header Bearer ....
    request= request.clone({
       setHeaders:{
-
         Authorization:`Bearer ${this.tokenService.getToken()}`
-
     }
   })
     console.log("request is  ==> ", request)
